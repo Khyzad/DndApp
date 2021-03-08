bf31a412defa478d3b989a5423b10513d36851be
Jingna Li <jingna13@gmail.com>
2021-03-08 14:17:38 -0800

    Use scss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CharacterSheet from './components/CharacterSheet';
 import {NavBar} from './components/NavBar/NavBar';
 
