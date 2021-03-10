8ebcf4e4b44e25d692a5cda2c5c267329992701c
khyzad <lljruffin@gmail.com>
2021-03-09 20:00:23 -0800

    Prototype: server boilerplate

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import config from './config';
+
+const { MONGO_URI, MONGO_DB_NAME } = config;
+
+const app = express();
+
+// CORS Middleware
+app.use(cors());
+// Logger Middleware
+app.use(morgan('dev'));
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = `${MONGO_URI}/${MONGO_DB_NAME}`;
+
+// Connect to Mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+
+// Serve static assets if in production
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+   console.log(`Server is running on port: ${port}`);
+});
+
+export default app;
